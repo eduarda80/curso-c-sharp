52d68c5a65c0a40681fbfe4a7ed6c33ae454cb07
Eduarda Camilli <eduarda@gerenesi.com.br>
2021-05-17 15:49:16 -0300

    *

--- CursoCSharp/CursoCSharp/MetodosEFuncoes/Recursividade.cs
+++ CursoCSharp/CursoCSharp/MetodosEFuncoes/Recursividade.cs
@@ -8,20 +8,13 @@ namespace CursoCSharp.MetodosEFuncoes
 {
     public sealed class Recursividade
     {
-        public static void Executar()
-        {
-            var dirPasta = @"C:\Nome da pasta".ParseHome();
-
-            var dirInfo = new DirectoryInfo(dirPasta);
-        }
-
         public void ExcluirDiretorio(string path)
         {
             if (!Directory.Exists(path))
                 return;
 
             var info = new DirectoryInfo(path);
-            
+
             //files: Arquivos (NÃO PASTAS, SOMENTE ARQUIVOS) do meu diretório
             var files = info.GetFiles();
 
@@ -38,32 +31,18 @@ namespace CursoCSharp.MetodosEFuncoes
             info.Delete();
         }
 
-        /*
-         Musicas
-            -Musica 0
-            -Rock    
-                -Musica 1
-                -Musica 2
-                -Musica 3
-                -Metal
-                    -Musica 4
-                    -Musica 5
-                    -Musica 6
-                -Romantico
-                   - Musica 7
-                    -Musica 8
-            Pop
-                MusicaX
-                ...
+        public static void Executar()
+        {
+           //string path = @"C:\temp\xmls";
 
-                MIchael
-                    ...
-                Maddona
-                    ...
-                Adele
-                    ...
-            Jazz
-                     
-         */
+           // if (!Path.GetFileName(path))
+           // {
+               
+           // }
+  
+           // ExcluirDiretorio(path);
+
+
+        }
     }
 }
